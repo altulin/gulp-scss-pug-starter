55865ac007a93af63e7e477b7969c0fde6e6bada
Alexander <al.tulin@gmail.com>
2023-06-28 16:44:51 +0800

    change sprite.js

--- gulp_modules/sprite.js
+++ gulp_modules/sprite.js
@@ -20,46 +20,50 @@ const {
 
 const createSprite = () => {
   deleteAsync(spriteFolder, { force: true });
-  return src(spriteFiles)
-    .pipe(imagemin([svgo()]))
+  return (
+    src(spriteFiles)
+      // .pipe(imagemin([svgo()]))
 
-    .pipe(
-      svgmin({
-        js2svg: {
-          pretty: true,
-          indent: 2,
-        },
-      })
-    )
+      .pipe(
+        svgmin({
+          js2svg: {
+            pretty: true,
+            indent: 2,
+          },
+          multipass: true,
+          plugins: [{ removeViewBox: false }],
+        })
+      )
 
-    .pipe(
-      cheerio({
-        run($) {
-          $("[fill]").removeAttr("fill");
-          $("[stroke]").removeAttr("stroke");
-          $("[style]").removeAttr("style");
-        },
-        parserOptions: { xmlMode: true },
-      })
-    )
+      .pipe(
+        cheerio({
+          run($) {
+            $("[fill]").removeAttr("fill");
+            $("[stroke]").removeAttr("stroke");
+            // $("[style]").removeAttr("style");
+          },
+          parserOptions: { xmlMode: true },
+        })
+      )
 
-    .pipe(
-      svgstore({
-        inlineSvg: true,
-      })
-    )
+      .pipe(
+        svgstore({
+          inlineSvg: true,
+        })
+      )
 
-    .pipe(replace("&gt;", ">"))
-    .pipe(replace("><defs", ">\n\t<defs"))
-    .pipe(replace("><symbol", ">\n\t<symbol"))
-    .pipe(replace("</symbol>", "\t</symbol>"))
-    .pipe(replace("<path", "\t<path"))
-    .pipe(replace("<g", "\t<g"))
-    .pipe(replace("</g>", "\t</g>"))
-    .pipe(replace("></svg", ">\n</svg"))
+      .pipe(replace("&gt;", ">"))
+      .pipe(replace("><defs", ">\n\t<defs"))
+      .pipe(replace("><symbol", ">\n\t<symbol"))
+      .pipe(replace("</symbol>", "\t</symbol>"))
+      .pipe(replace("<path", "\t<path"))
+      .pipe(replace("<g", "\t<g"))
+      .pipe(replace("</g>", "\t</g>"))
+      .pipe(replace("></svg", ">\n</svg"))
 
-    .pipe(rename(spriteFile))
-    .pipe(dest(imgFolder));
+      .pipe(rename(spriteFile))
+      .pipe(dest(imgFolder))
+  );
 };
 
 export default createSprite;
